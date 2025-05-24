0e18a7440ddbb7b79cdf5caa51fd77eae2e17a3c
ericafk0001 <dxslyofficial@gmail.com>
2025-05-24 17:40:35 -0400

    done

--- script.js
+++ script.js
@@ -39,8 +39,50 @@ const thingText = document.getElementById("thing");
 const somethingText = document.getElementById("something");
 const languageText = document.getElementById("language");
 
-generateBtn.addEventListener("click", function () {
-  thingText.innerText = pickRandom(things);
-  somethingText.innerText = pickRandom(something);
-  languageText.innerText = pickRandom(languages);
+const spinningSound = new Audio("sounds/spinning.mp3");
+spinningSound.loop = true;
+const stopSound = new Audio("sounds/stop.mp3");
+
+function animateText(element, array, finalValue, delay = 0) {
+  return new Promise((resolve) => {
+    let iterations = 0;
+    const maxIterations = 20;
+
+    setTimeout(() => {
+      const interval = setInterval(() => {
+        element.innerText = pickRandom(array);
+        iterations++;
+
+        if (iterations === maxIterations) {
+          clearInterval(interval);
+          element.innerText = finalValue;
+          resolve();
+        }
+      }, 50);
+    }, delay);
+  });
+}
+
+generateBtn.addEventListener("click", async function () {
+  generateBtn.disabled = true;
+  spinningSound.volume = 0.5; // Adjust volume if needed
+  stopSound.volume = 0.3; // Adjust volume if needed
+
+  spinningSound.play();
+
+  const selectedThing = pickRandom(things);
+  const selectedSomething = pickRandom(something);
+  const selectedLanguage = pickRandom(languages);
+
+  await Promise.all([
+    animateText(thingText, things, selectedThing, 0),
+    animateText(somethingText, something, selectedSomething, 1100),
+    animateText(languageText, languages, selectedLanguage, 2100),
+  ]);
+
+  spinningSound.pause();
+  spinningSound.currentTime = 0;
+  stopSound.play();
+
+  generateBtn.disabled = false;
 });
